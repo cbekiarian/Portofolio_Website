2c64fc5248dfe105b30ae5a48983333fb4bc2e66
cbekiarian <44470872+cbekiarian@users.noreply.github.com>
2024-04-09 10:35:07 +0300

    flights completed to a working degree

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from datetime import date
+
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
-from flask_ckeditor import CKEditor
-from flask_gravatar import Gravatar
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text
-from functools import wraps
-from werkzeug.security import generate_password_hash, check_password_hash
 from flights.FlightFinder import SearchFlights
 from forms.airplane_form import AirplaneForm
 import os
